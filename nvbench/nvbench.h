a942f115aa6a235e3aa783148218c1eadcf544c6
Allison Vacanti <alliepiper16@gmail.com>
2020-10-18 08:19:25 -0400

    cuda_timer: add ready(), refactor member vars, stream support.

--- nvbench/nvbench.h
+++ nvbench/nvbench.h
@@ -1,6 +1,7 @@
 // TODO license. I'd like to make this BSD 3-clause for simplicity.
 // TODO Need a way to select device at runtime.
 // TODO Need to customize reported times more: Wall *and/or* CPU *and/or* GPU
+// TODO Implement GPU-time only benchmarks.
 
 #pragma once
 
@@ -29,32 +30,45 @@ namespace nvbench
 // Modified from existing thrust benchmark code:
 class cuda_timer
 {
-  cudaEvent_t start_;
-  cudaEvent_t stop_;
+  cudaEvent_t m_start;
+  cudaEvent_t m_stop;
+  cudaStream_t m_stream;
 
 public:
-  cuda_timer()
+  cuda_timer(cudaStream_t stream = 0)
+      : m_stream(stream)
   {
-    CUDA_CALL(cudaEventCreate(&start_));
-    CUDA_CALL(cudaEventCreate(&stop_));
+    CUDA_CALL(cudaEventCreate(&m_start));
+    CUDA_CALL(cudaEventCreate(&m_stop));
   }
 
   ~cuda_timer()
   {
-    CUDA_CALL(cudaEventDestroy(start_));
-    CUDA_CALL(cudaEventDestroy(stop_));
+    CUDA_CALL(cudaEventDestroy(m_start));
+    CUDA_CALL(cudaEventDestroy(m_stop));
   }
 
-  void start() { CUDA_CALL(cudaEventRecord(start_, 0)); }
+  void start() { CUDA_CALL(cudaEventRecord(m_start, m_stream)); }
 
-  void stop() { CUDA_CALL(cudaEventRecord(stop_, 0)); }
+  void stop() { CUDA_CALL(cudaEventRecord(m_stop, m_stream)); }
+
+  bool ready()
+  {
+    cudaError_t const state = cudaEventQuery(m_stop);
+    if (state == cudaErrorNotReady)
+    {
+      return false;
+    }
+    CUDA_CALL(state);
+    return true;
+  }
 
   double milliseconds_elapsed()
   {
-    CUDA_CALL(cudaEventSynchronize(start_));
-    CUDA_CALL(cudaEventSynchronize(stop_));
+    CUDA_CALL(cudaEventSynchronize(m_start));
+    CUDA_CALL(cudaEventSynchronize(m_stop));
     float elapsed_time;
-    CUDA_CALL(cudaEventElapsedTime(&elapsed_time, start_, stop_));
+    CUDA_CALL(cudaEventElapsedTime(&elapsed_time, m_start, m_stop));
     return elapsed_time;
   }
 
