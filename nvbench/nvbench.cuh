238cd3e7733638c867162f31909ec7f13ff9996b
Allison Vacanti <alliepiper16@gmail.com>
2021-01-01 02:10:51 -0500

    Add main nvbench.cuh header.

---
+++ nvbench/nvbench.cuh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <nvbench/benchmark.cuh>
+#include <nvbench/benchmark_base.cuh>
+#include <nvbench/benchmark_manager.cuh>
+#include <nvbench/callable.cuh>
+#include <nvbench/cpu_timer.cuh>
+#include <nvbench/create.cuh>
+#include <nvbench/cuda_call.cuh>
+#include <nvbench/cuda_stream.cuh>
+#include <nvbench/cuda_timer.cuh>
+#include <nvbench/main.cuh>
+#include <nvbench/state.cuh>
+#include <nvbench/type_list.cuh>
+#include <nvbench/types.cuh>
