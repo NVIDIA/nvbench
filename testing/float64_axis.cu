36de02d2bec4ad4520ddbcaa2113c9b885dc391a
Allison Vacanti <alliepiper16@gmail.com>
2021-02-03 12:50:50 -0500

    Implement/test mutable float64_axis.

--- testing/float64_axis.cu
+++ testing/float64_axis.cu
@@ -61,8 +61,86 @@ void test_basic()
   ASSERT(clone->get_description(2) == "");
 }
 
+void test_updates()
+{
+  nvbench::float64_axis axis("Basic");
+  axis.set_inputs({-100.3, 0., 2064.15});
+
+  ASSERT(axis.get_name() == "Basic");
+  ASSERT(axis.get_size() == 3);
+  ASSERT(axis.get_value(0) == -100.3);
+  ASSERT(axis.get_input_string(0) == "-100.3");
+  ASSERT(axis.get_description(0) == "");
+  ASSERT(axis.get_value(1) == 0.);
+  ASSERT(axis.get_input_string(1) == "0");
+  ASSERT(axis.get_description(1) == "");
+  ASSERT(axis.get_value(2) == 2064.15);
+  ASSERT(axis.get_input_string(2) == "2064.15");
+  ASSERT(axis.get_description(2) == "");
+
+  auto clone_base = axis.clone();
+  ASSERT(clone_base.get() != nullptr);
+  auto *clone = dynamic_cast<nvbench::float64_axis *>(clone_base.get());
+  ASSERT(clone != nullptr);
+
+  ASSERT(clone->get_name() == "Basic");
+  ASSERT(clone->get_size() == 3);
+  ASSERT(clone->get_value(0) == -100.3);
+  ASSERT(clone->get_input_string(0) == "-100.3");
+  ASSERT(clone->get_description(0) == "");
+  ASSERT(clone->get_value(1) == 0.);
+  ASSERT(clone->get_input_string(1) == "0");
+  ASSERT(clone->get_description(1) == "");
+  ASSERT(clone->get_value(2) == 2064.15);
+  ASSERT(clone->get_input_string(2) == "2064.15");
+  ASSERT(clone->get_description(2) == "");
+
+  // Clear the clone:
+  clone->set_inputs({});
+  ASSERT(clone->get_name() == "Basic");
+  ASSERT(clone->get_size() == 0);
+
+  // Original axis should be unaffected:
+  ASSERT(axis.get_name() == "Basic");
+  ASSERT(axis.get_size() == 3);
+  ASSERT(axis.get_value(0) == -100.3);
+  ASSERT(axis.get_input_string(0) == "-100.3");
+  ASSERT(axis.get_description(0) == "");
+  ASSERT(axis.get_value(1) == 0.);
+  ASSERT(axis.get_input_string(1) == "0");
+  ASSERT(axis.get_description(1) == "");
+  ASSERT(axis.get_value(2) == 2064.15);
+  ASSERT(axis.get_input_string(2) == "2064.15");
+  ASSERT(axis.get_description(2) == "");
+
+  // Insert new data:
+  clone->set_inputs({3.14, 6.28});
+  ASSERT(clone->get_name() == "Basic");
+  ASSERT(clone->get_size() == 2);
+  ASSERT(clone->get_value(0) == 3.14);
+  ASSERT(clone->get_input_string(0) == "3.14");
+  ASSERT(clone->get_description(0) == "");
+  ASSERT(clone->get_value(1) == 6.28);
+  ASSERT(clone->get_input_string(1) == "6.28");
+  ASSERT(clone->get_description(1) == "");
+
+  // Original axis should be unaffected:
+  ASSERT(axis.get_name() == "Basic");
+  ASSERT(axis.get_size() == 3);
+  ASSERT(axis.get_value(0) == -100.3);
+  ASSERT(axis.get_input_string(0) == "-100.3");
+  ASSERT(axis.get_description(0) == "");
+  ASSERT(axis.get_value(1) == 0.);
+  ASSERT(axis.get_input_string(1) == "0");
+  ASSERT(axis.get_description(1) == "");
+  ASSERT(axis.get_value(2) == 2064.15);
+  ASSERT(axis.get_input_string(2) == "2064.15");
+  ASSERT(axis.get_description(2) == "");
+}
+
 int main()
 {
   test_empty();
   test_basic();
+  test_updates();
 }
