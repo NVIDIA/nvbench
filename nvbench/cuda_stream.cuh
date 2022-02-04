33a896f99eab7001efead4f23132978d56b43c69
Yunsong Wang <yunsongw@nvidia.com>
2022-02-04 17:25:50 -0500

    Update copyright year

--- nvbench/cuda_stream.cuh
+++ nvbench/cuda_stream.cuh
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2021 NVIDIA Corporation
+ *  Copyright 2021-2022 NVIDIA Corporation
  *
  *  Licensed under the Apache License, Version 2.0 with the LLVM exception
  *  (the "License"); you may not use this file except in compliance with
