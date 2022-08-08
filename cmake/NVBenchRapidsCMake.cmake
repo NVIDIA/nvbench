8919728d329c0af0cca1fc839d70e8ef38247d0e
Robert Maynard <rmaynard@nvidia.com>
2022-08-08 13:23:45 -0400

    Update to latest version of rapids
    Also ensure that we don't clobber any existing rapids.cmake file

--- cmake/NVBenchRapidsCMake.cmake
+++ cmake/NVBenchRapidsCMake.cmake
@@ -1,10 +1,12 @@
 # Called before project(...)
 macro(nvbench_load_rapids_cmake)
-  file(DOWNLOAD
-    https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-21.12/RAPIDS.cmake
-    "${CMAKE_BINARY_DIR}/RAPIDS.cmake"
-  )
-  include("${CMAKE_BINARY_DIR}/RAPIDS.cmake")
+  if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/NVBENCH_RAPIDS.cmake")
+    file(DOWNLOAD
+      https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-22.08/RAPIDS.cmake
+      "${CMAKE_CURRENT_BINARY_DIR}/NVBENCH_RAPIDS.cmake"
+    )
+  endif()
+  include("${CMAKE_CURRENT_BINARY_DIR}/NVBENCH_RAPIDS.cmake")
 
   include(rapids-cmake)
   include(rapids-cpm)
