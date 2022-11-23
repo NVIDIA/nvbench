2b8c482308482230a6af7b77c950431dab321028
Aaron Siddhartha Mondal <aaron@eomii.org>
2022-11-24 00:44:55 +0100

    Fix wrongly defaulted move assignment operators
    Found because -Wdefaulted-function-deleted complained.

--- nvbench/printer_base.cuh
+++ nvbench/printer_base.cuh
@@ -79,7 +79,7 @@ struct printer_base
   printer_base(const printer_base &)            = delete;
   printer_base(printer_base &&)                 = default;
   printer_base &operator=(const printer_base &) = delete;
-  printer_base &operator=(printer_base &&)      = default;
+  printer_base &operator=(printer_base &&)      = delete;
 
   /*!
    * Called once with the command line arguments used to invoke the current
