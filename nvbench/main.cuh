04aa9ce00e50b0119ec3e5d33e6ba29fa8ad54d0
David Wendt <dwendt@nvidia.com>
2023-02-01 13:35:21 -0500

    Add optional global setup/teardown feature to main

--- nvbench/main.cuh
+++ nvbench/main.cuh
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2021 NVIDIA Corporation
+ *  Copyright 2021-2023 NVIDIA Corporation
  *
  *  Licensed under the Apache License, Version 2.0 with the LLVM exception
  *  (the "License"); you may not use this file except in compliance with
@@ -54,6 +54,12 @@
 // clang-format on
 #endif
 
+#ifndef NVBENCH_ENVIRONMENT
+struct no_environment
+{};
+#define NVBENCH_ENVIRONMENT no_environment
+#endif
+
 #define NVBENCH_MAIN_PARSE(argc, argv)                                                             \
   nvbench::option_parser parser;                                                                   \
   parser.parse(argc, argv)
@@ -77,6 +83,7 @@
     printer.set_total_state_count(total_states);                                                   \
                                                                                                    \
     printer.set_completed_state_count(0);                                                          \
+    NVBENCH_ENVIRONMENT{};                                                                         \
     for (auto &bench_ptr : benchmarks)                                                             \
     {                                                                                              \
       bench_ptr->set_printer(printer);                                                             \
