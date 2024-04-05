78a265ea55645f6397046de32f042fefff0d59fd
Allison Piper <alliepiper16@gmail.com>
2024-04-05 17:34:00 +0000

    Support CLI CMake options for windows ci scripts.

--- ci/windows/test_nvbench.ps1
+++ ci/windows/test_nvbench.ps1
@@ -4,7 +4,11 @@ Param(
     [Alias("std")]
     [ValidateNotNullOrEmpty()]
     [ValidateSet(17)]
-    [int]$CXX_STANDARD = 17
+    [int]$CXX_STANDARD = 17,
+    [Parameter(Mandatory = $false)]
+    [Alias("cmake-options")]
+    [ValidateNotNullOrEmpty()]
+    [int]$ARG_CMAKE_OPTIONS = ""
 )
 
 $CURRENT_PATH = Split-Path $pwd -leaf
@@ -18,6 +22,11 @@ Import-Module $PSScriptRoot/build_common.psm1 -ArgumentList $CXX_STANDARD
 
 $PRESET = "nvbench-cpp$CXX_STANDARD"
 $CMAKE_OPTIONS = ""
+
+# Append any arguments pass in on the command line
+If($ARG_CMAKE_OPTIONS -ne "") {
+    $CMAKE_OPTIONS += "$ARG_CMAKE_OPTIONS"
+}
 
 configure_and_build_preset "NVBench" "$PRESET" "$CMAKE_OPTIONS"
 test_preset "NVBench" "$PRESET"
