974744de11030a01d7ae4276086b1b5fee4340aa
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-05-22 09:51:47 -0500

    Update json and fmt projects to latest versions

--- cmake/NVBenchDependencies.cmake
+++ cmake/NVBenchDependencies.cmake
@@ -8,11 +8,11 @@ if(NOT BUILD_SHARED_LIBS AND NVBench_ENABLE_INSTALL_RULES)
   set(install_fmt ON)
 endif()
 
-rapids_cpm_find(fmt 11.1.4 ${export_set_details}
+rapids_cpm_find(fmt 11.2.0 ${export_set_details}
   GLOBAL_TARGETS fmt::fmt fmt::fmt-header-only
   CPM_ARGS
     GIT_REPOSITORY "https://github.com/fmtlib/fmt.git"
-    GIT_TAG "11.1.4"
+    GIT_TAG "11.2.0"
     OPTIONS
       # Force static to keep fmt internal.
       "BUILD_SHARED_LIBS OFF"
@@ -33,10 +33,10 @@ endif()
 # Following recipe from
 # http://github.com/cpm-cmake/CPM.cmake/blob/master/examples/json/CMakeLists.txt
 # Download the zips because the repo takes an excessively long time to clone.
-rapids_cpm_find(nlohmann_json 3.11.3
+rapids_cpm_find(nlohmann_json 3.12.0
   CPM_ARGS
-    URL https://github.com/nlohmann/json/releases/download/v3.11.3/include.zip
-    URL_HASH SHA256=a22461d13119ac5c78f205d3df1db13403e58ce1bb1794edc9313677313f4a9d
+    URL https://github.com/nlohmann/json/releases/download/v3.12.0/include.zip
+    URL_HASH SHA256=b8cb0ef2dd7f57f18933997c9934bb1fa962594f701cd5a8d3c2c80541559372
   PATCH_COMMAND
     ${CMAKE_COMMAND}
       -D "CUDA_VERSION=${CMAKE_CUDA_COMPILER_VERSION}"
