361c0337bee6b238ff55ed01cbc9df9bc5b73cc0
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-07-22 16:33:48 -0500

    Use cuda-pathfinder instead of cuda-bindings for Pathfinder
    Removed use of __all__ per PR feedback. Emit warnings.warn if
    version information could not be retrieved from the package metadata,
    e.g., package has been renamed by source code was not updated.

--- python/cuda/nvbench/__init__.py
+++ python/cuda/nvbench/__init__.py
@@ -1,31 +1,40 @@
 import importlib.metadata
+import warnings
 
-from cuda.bindings.path_finder import (  # type: ignore[import-not-found]
-    _load_nvidia_dynamic_library,
+from cuda.pathfinder import (  # type: ignore[import-not-found]
+    load_nvidia_dynamic_lib,
 )
 
 try:
     __version__ = importlib.metadata.version("pynvbench")
-except Exception:
+except Exception as e:
     __version__ = "0.0.0dev"
+    warnings.warn(
+        "Could not retrieve version of pynvbench package dynamically from its metadata. "
+        f"Exception {e} was raised. "
+        f"Version is set to fall-back value '{__version__}' instead."
+    )
 
 for libname in ("cupti", "nvperf_target", "nvperf_host"):
-    _load_nvidia_dynamic_library(libname)
+    load_nvidia_dynamic_lib(libname)
 
 from ._nvbench import (  # noqa: E402
-    Benchmark,
-    CudaStream,
-    Launch,
-    State,
-    register,
-    run_all_benchmarks,
+    Benchmark as Benchmark,
+)
+from ._nvbench import (  # noqa: E402
+    CudaStream as CudaStream,
+)
+from ._nvbench import (  # noqa: E402
+    Launch as Launch,
+)
+from ._nvbench import (  # noqa: E402
+    State as State,
+)
+from ._nvbench import (  # noqa: E402
+    register as register,
+)
+from ._nvbench import (  # noqa: E402
+    run_all_benchmarks as run_all_benchmarks,
 )
 
-__all__ = [
-    "register",
-    "run_all_benchmarks",
-    "CudaStream",
-    "Launch",
-    "State",
-    "Benchmark",
-]
+del load_nvidia_dynamic_lib
