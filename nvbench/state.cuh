a33a454a2d4fc8ab08e8e36d2bfbc27da19ee7c4
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2026-02-02 14:42:07 -0600

    Make skip_hot_measurement method const

--- nvbench/state.cuh
+++ nvbench/state.cuh
@@ -306,7 +306,7 @@ private:
         std::optional<nvbench::device_info> device,
         std::size_t type_config_index);
 
-  bool skip_hot_measurement() { return get_run_once() || get_skip_batched(); }
+  [[nodiscard]] bool skip_hot_measurement() const { return get_run_once() || get_skip_batched(); }
 
   std::reference_wrapper<const nvbench::benchmark_base> m_benchmark;
   nvbench::named_values m_axis_values;
